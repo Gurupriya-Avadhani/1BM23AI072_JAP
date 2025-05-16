5221845f6fe60061cc9242e538a2bb1ab65f1365
Gurupriya-Avadhani <gurupriya.ai23@bmsce.ac.in>
2025-05-16 22:34:37 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+package apack;
+
+public class A {
+    int defaultVar = 1;          // default access
+    protected int protectedVar = 2;
+    private int privateVar = 3;
+    public int publicVar = 4;
+}
